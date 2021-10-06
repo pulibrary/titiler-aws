3fdcce1df4b2d169f23e15abe01be6251ae612b3
Eliot Jordan <eliotj@princeton.edu>
2021-10-06 12:51:57 -0500

    Updates for EC2

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ TitilerEc2Stack(app, "TitilerEc2Stack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
--- titiler_ec2/titiler_ec2_stack.py
+++ titiler_ec2/titiler_ec2_stack.py
@@ -1,15 +1,59 @@
-from aws_cdk import core as cdk
+from pathlib import Path
+from aws_cdk import (core, aws_ec2 as ec2, aws_iam as iam)
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+class TitilerEc2Stack(core.Stack):
 
-
-class TitilerEc2Stack(cdk.Stack):
-
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # VPC
+        vpc = ec2.Vpc(self, "tiler-cdk-vpc",
+            cidr="10.0.0.0/16",
+            nat_gateways=0,
+            subnet_configuration=[ec2.SubnetConfiguration(name="public",cidr_mask=24,subnet_type=ec2.SubnetType.PUBLIC)]
+            )
+
+        # Security Group
+        sg = ec2.SecurityGroup(self, "tiler-sg",
+            vpc=vpc,
+            description="SecurityGroup for titiler",
+            security_group_name="Tiler SecurityGroup",
+            allow_all_outbound=True,
+            )
+
+        sg.add_ingress_rule(ec2.Peer.any_ipv4(),
+            ec2.Port.tcp(22), "allow ssh access from anywhere")
+
+        sg.add_ingress_rule(ec2.Peer.any_ipv4(),
+            ec2.Port.tcp(80), "allow HTTP traffic from anywhere")
+
+        sg.add_ingress_rule(ec2.Peer.any_ipv4(),
+            ec2.Port.tcp(443), "allow HTTPS traffic from anywhere")
+
+        # Role
+        role = iam.Role(self, "tiler-role", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess"))
+
+        # Instance
+        ubuntu_linux = ec2.MachineImage.generic_linux({
+            "us-east-2": "ami-08853a6c93b952e8b"
+            })
+
+        instance = ec2.Instance(self, "titiler-instance",
+            machine_image = ubuntu_linux,
+            instance_type = ec2.InstanceType("t3.micro"),
+            vpc = vpc,
+            vpc_subnets = ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+            role = role,
+            security_group = sg,
+            key_name = 'titiler-key-pair'
+            )
+
+        # User Data
+        script = Path('configure.sh').read_text()
+        instance.add_user_data(script)
+
+        # Messages after deploy
+        output = core.CfnOutput(self, "WebHost_information",
+            value=instance.instance_public_ip,
+            description="Web server's Public IP")
