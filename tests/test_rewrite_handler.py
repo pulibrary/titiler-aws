23e529c75ff2cf83e9c3bbae4ee86d48a4d0a269
Trey Pendragon <tpendragon@princeton.edu>
2021-12-16 11:50:13 -0800

    Add cog URIs

--- tests/test_rewrite_handler.py
+++ tests/test_rewrite_handler.py
@@ -3,12 +3,23 @@ import pytest
 from resources.rewrite_handler_production import handler as handler_production
 from resources.rewrite_handler_staging import handler as handler_staging
 
-def test_rewrites_id_production():
+def test_rewrites_mosaic_id_production():
     event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/mosaicjson', 'querystring': 'id=banana'}}}]}
     output = handler_production(event, {})
     assert output['querystring'] == 'id=banana&url=s3%3A%2F%2Ffiggy-geo-production%2Fba%2Fna%2Fna%2Fbanana%2Fmosaic.json'
 
-def test_rewrites_id_staging():
+
+def test_rewrites_cog_id_production():
+    event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/cog', 'querystring': 'id=banana'}}}]}
+    output = handler_production(event, {})
+    assert output['querystring'] == 'id=banana&url=s3%3A%2F%2Ffiggy-geo-production%2Fba%2Fna%2Fna%2Fbanana%2Fdisplay_raster.tif'
+
+def test_rewrites_mosaic_id_staging():
     event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/mosaicjson', 'querystring': 'id=banana'}}}]}
     output = handler_staging(event, {})
     assert output['querystring'] == 'id=banana&url=s3%3A%2F%2Ffiggy-geo-staging%2Fba%2Fna%2Fna%2Fbanana%2Fmosaic.json'
+
+def test_rewrites_cog_id_staging():
+    event = {'Records': [{'cf': {'request': {'uri': 'https://test.com/cog', 'querystring': 'id=banana'}}}]}
+    output = handler_production(event, {})
+    assert output['querystring'] == 'id=banana&url=s3%3A%2F%2Ffiggy-geo-production%2Fba%2Fna%2Fna%2Fbanana%2Fdisplay_raster.tif'
