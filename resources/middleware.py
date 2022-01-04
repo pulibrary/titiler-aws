7a1b00a97a32d3d4f914a5e78782d86bbcf98c43
Trey Pendragon <tpendragon@princeton.edu>
2022-01-03 17:08:57 -0800

    Don't error if it's not HTTP, just fall through.
    Also restrict to 1 thread, prevents rio tiler from erroring. See
    https://github.com/developmentseed/titiler/issues/409

--- resources/middleware.py
+++ resources/middleware.py
@@ -5,7 +5,11 @@ class HostMiddleware:
         self.app = app
 
     async def __call__(self, scope, receive, send):
-        assert scope["type"] == "http"
+        if scope["type"] not in (
+          "http"
+        ):
+            await self.app(scope, receive, send)
+            return
         headers = dict(scope["headers"])
         headers[b"host"] = bytes(os.getenv("TITILER_BASE_URL"), 'utf-8')
         scope["headers"] = [(k, v) for k, v in headers.items()]
